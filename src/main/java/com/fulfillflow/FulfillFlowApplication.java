10a941d3ec52b5b013c30450c91f4a9cc40c2c8a
jonathanphuung <jonathanphuung@gmail.com>
2026-07-17 19:08:33 -0700

    init spring api

---
+++ src/main/java/com/fulfillflow/FulfillFlowApplication.java
@@ -0,0 +1,13 @@
+package com.fulfillflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FulfillFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FulfillFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fulfillflow/FulfillFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fulfillflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FulfillFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
